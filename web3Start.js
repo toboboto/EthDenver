273e528def2e72a290bd22c9b513c580922489af
toboboto <toboboto@gmail.com>
2024-03-02 03:15:45 -0700

    Update web3Start.js.demo.02

--- web3Start.js
+++ web3Start.js
@@ -5,35 +5,30 @@
     {
       "name": "For Ever Wall- FEW"
       "description": "",
-      "photo": "url('aradicaldecentralization.png')",
+      "photo": "url('fewwall.png')",
       "url": "https://ipfs.io/ipfs/QmaMzwPXUGviEYk3uu2CqPBGnUmBgXFxRP8kJXFbbjSnCq",
-      "body": "There's only two of us and we didn't find a front end dev, so Forever Wall is FEW.  Access and save data with hashtags into the blockchain in the most decentralized way possible" 
+      "body": "There's only two of us and we didn't find a front end dev. Keeping with the concept of Keep It Super Simple (KISS), Forever Wall was named FEW." 
     },
     {
-      "name": "WEB3 is currently building WEB2.5",
+      "name": "'WEB3' is currently building 'WEB2.5'",
       "photo": "url('circuitSuit.png')",
       "url": "chick-image.html",
       "description": "WEB2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
     },
     {
       "name":"The cost of convenience is too large",
-      "description":"These centralized elements introduce complexity and potential for censorship",
+      "description":"These centralized elements introduce complexity and potential for censorship", 
       "photo":"url('chainbreak.png')",
     },
     {
       "name":"True WEB3",
-      "description":"Digital transformation from 2.5 to 3.0 to manifest true WEB3, we decentralize everything. Serverless architecture for front end that is decentralized.  Also, on chain low level index eliminates external search tables.",
+      "description":"Digital transformation from 2.5 to 3.0 to manifest true WEB3,  we access and save data with hashtags into the blockchain in the most decentralized way possible to decentralize everything. Serverless architecture for front end that is decentralized.  Also, on chain low level index eliminates external search tables.",
       "photo":"url('worldchain.png')",
     },
     {
       "name":"Friend with Benefits", 
       "description":"- On chain versioning, Apply bug fixes simply, Improve production speed",
       "photo":"url('benefits.png')",
-    }
-    {
-      "name":"Real WEB3 now!", 
-      "description":"We're using real WEB3 now",
-      "photo":"url('pinnochio.png')",
     }
     {
       "name":"A FEW KISSes",
