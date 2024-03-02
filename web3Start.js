6e2c5ac320ff10d1f291462a9db98ccd32408fdf
toboboto <toboboto@gmail.com>
2024-03-02 01:39:45 -0700

    Update web3Start.js

--- web3Start.js
+++ web3Start.js
@@ -5,37 +5,44 @@
     {
       "name": "Simply Radical Decentralization"// summary "Decentralization X Simplicity", // kiss FEW forever wall
       "description": "",
-      "photo": "url('world.png?asd')",
+      "photo": "url('RadicalDecentralization.png')",
       "url": "https://ipfs.io/ipfs/QmaMzwPXUGviEYk3uu2CqPBGnUmBgXFxRP8kJXFbbjSnCq",
-      "body": "Access and save data with hashtags into the blockchain in teh most decentralized way possible" //few kiss
+      "body": "Access and save data with hashtags into the blockchain in the most decentralized way possible" //few kiss
     },
     {
       "name": "We're building in web 2.5", //what is problem
       "description": "Scan your special code stamps into a secure website.",
       //"body": "Within the tiny Web3 'early adopter' community, special Self-Sovereign software is used. A package called Metamask(metamask.io) is quite popular. But most people are not interested in installing more applications onto their phone or computer. That is why we developed this easy QR code system. We believe that these identity kits (with little QR code stickers) are just as secure as the industry standard solutions. They also allow you to experience the new Web3 without committing to a new piece of software.",
-      "photo": "url('qrcode.jpg?23')",
+      "photo": "url('circuitSuit.png')",
       "url": "chick-image.html",
-      "description: "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
+      "description": "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
     },
     {
       "name":"The cost of convenience is too large", // why is a problem
       //"description":"This is a Web3 hosted site. The URL that you are visiting should begin with 'ipfs.io'. ",
       "description":"These centralized elements introduce complexity and potential for censorship",
-      "photo":"url('world.png')",
+      "photo":"url('chainbreak.png')",
       //"button": "Learn more about IPFS"
     },
     {
-      "name":"Hmm? I am still not convinced!",
+      "name":"true web 3 HOW HOW HOW", // how we fix it
       //"description":"Not only can you know that this site never changes. It is also written to be read and audited. Experts will be needed to certify each version.",
-      "description":"This is a huge work in process. We need beta testers from all sorts of backgrounds.",
-      "photo":"url('world.png')",
+      "description":"Digital transformation from 2.5 to 3.0 to manifest true WEB 3, we decentralize everything. Serverless architecture for front end that is decentralized.  Also, on chain low level index eliminates external search tables.",
+      "photo":"url('world chain.png')",
       //"button":"What can I do to help?"
     },
     {
-      "name":"What this site provides.",
+      "name":"BENEFITS BENEFITS ",  // benefits
       //"body":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
-      "description":"When you assemble four of the stickers into a row and scan them with a phone or laptop, an Ethereum-style blockchain address is created. While the technology came to prominence with bitcoin, the technology is far more than a money system.",
-      "photo":"url('world.png')",
+      "description":"- On chain versioning, Apply bug fixes simply, Improve production speed",
+      "photo":"url('benefits.png')",
+      //"button":"Learn about blockchain"
+    }
+    {
+      "name":"Real WEB3 now!",  // benefits
+      //"body":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
+      "description":"We're using real WEB3 now",
+      "photo":"url('pinnochio.png')",
       //"button":"Learn about blockchain"
     }
   ]
