9fcc1c337f71c65c7e74a4806b5e9ae625c00da1
toboboto <toboboto@gmail.com>
2024-03-02 01:26:28 -0700

    Update web3Start.js
    changed body to description

--- web3Start.js
+++ web3Start.js
@@ -15,28 +15,28 @@
       //"body": "Within the tiny Web3 'early adopter' community, special Self-Sovereign software is used. A package called Metamask(metamask.io) is quite popular. But most people are not interested in installing more applications onto their phone or computer. That is why we developed this easy QR code system. We believe that these identity kits (with little QR code stickers) are just as secure as the industry standard solutions. They also allow you to experience the new Web3 without committing to a new piece of software.",
       "photo": "url('qrcode.jpg?23')",
       "url": "chick-image.html",
-      "body": "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
+      "description: "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
     },
     {
-      "name":"why why why", // why is a problem
-      "description":"This is a Web3 hosted site. The URL that you are visiting should begin with 'ipfs.io'. ",
-      "body":"We are used to businesses and people asking us to trust them based on some record of good behaviour. The Interplanetary Filesystem (IPFS) is an example of a 'trustless' Web3 technology. You have likely been trained to only visit and trust HTTPS websites. You can look at your browser to see this. IPFS takes this one step further. An IPFS website (https://ipfs.io/) uses a new 'Content-Based' addressing system. So if one character on this page is changed, the entire address will change.",
+      "name":"The cost of convenience is too large", // why is a problem
+      //"description":"This is a Web3 hosted site. The URL that you are visiting should begin with 'ipfs.io'. ",
+      "description":"These centralized elements introduce complexity and potential for censorship",
       "photo":"url('world.png')",
       //"button": "Learn more about IPFS"
     },
     {
       "name":"Hmm? I am still not convinced!",
-      "description":"Not only can you know that this site never changes. It is also written to be read and audited. Experts will be needed to certify each version.",
-      "body":"This is a huge work in process. We need beta testers from all sorts of backgrounds.",
+      //"description":"Not only can you know that this site never changes. It is also written to be read and audited. Experts will be needed to certify each version.",
+      "description":"This is a huge work in process. We need beta testers from all sorts of backgrounds.",
       "photo":"url('world.png')",
-      "button":"What can I do to help?"
+      //"button":"What can I do to help?"
     },
     {
       "name":"What this site provides.",
-      "description":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
-      "body":"When you assemble four of the stickers into a row and scan them with a phone or laptop, an Ethereum-style blockchain address is created. While the technology came to prominence with bitcoin, the technology is far more than a money system.",
+      //"body":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
+      "description":"When you assemble four of the stickers into a row and scan them with a phone or laptop, an Ethereum-style blockchain address is created. While the technology came to prominence with bitcoin, the technology is far more than a money system.",
       "photo":"url('world.png')",
-      "button":"Learn about blockchain"
+      //"button":"Learn about blockchain"
     }
   ]
 }
