046970978f0f5c9690dc6af293479141f4255f0c
toboboto <toboboto@gmail.com>
2024-03-02 02:42:08 -0700

    Update web3Start.js.demo.01
    images named, comments cleaned out, KISS FEW added

--- web3Start.js
+++ web3Start.js
@@ -3,47 +3,42 @@
   "heading": "Intro",
   "slides": [
     {
-      "name": "Simply Radical Decentralization"// summary "Decentralization X Simplicity", // kiss FEW forever wall
+      "name": "For Ever Wall- FEW"
       "description": "",
-      "photo": "url('RadicalDecentralization.png')",
+      "photo": "url('aradicaldecentralization.png')",
       "url": "https://ipfs.io/ipfs/QmaMzwPXUGviEYk3uu2CqPBGnUmBgXFxRP8kJXFbbjSnCq",
-      "body": "Access and save data with hashtags into the blockchain in the most decentralized way possible" //few kiss
+      "body": "There's only two of us and we didn't find a front end dev, so Forever Wall is FEW.  Access and save data with hashtags into the blockchain in the most decentralized way possible" //few kiss
     },
     {
-      "name": "We're building in web 2.5", //what is problem
-      "description": "Scan your special code stamps into a secure website.",
-      //"body": "Within the tiny Web3 'early adopter' community, special Self-Sovereign software is used. A package called Metamask(metamask.io) is quite popular. But most people are not interested in installing more applications onto their phone or computer. That is why we developed this easy QR code system. We believe that these identity kits (with little QR code stickers) are just as secure as the industry standard solutions. They also allow you to experience the new Web3 without committing to a new piece of software.",
+      "name": "WEB3 is currently building WEB2.5",
       "photo": "url('circuitSuit.png')",
       "url": "chick-image.html",
-      "description": "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
+      "description": "WEB2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
     },
     {
-      "name":"The cost of convenience is too large", // why is a problem
-      //"description":"This is a Web3 hosted site. The URL that you are visiting should begin with 'ipfs.io'. ",
+      "name":"The cost of convenience is too large",
       "description":"These centralized elements introduce complexity and potential for censorship",
       "photo":"url('chainbreak.png')",
-      //"button": "Learn more about IPFS"
     },
     {
-      "name":"true web 3 HOW HOW HOW", // how we fix it
-      //"description":"Not only can you know that this site never changes. It is also written to be read and audited. Experts will be needed to certify each version.",
-      "description":"Digital transformation from 2.5 to 3.0 to manifest true WEB 3, we decentralize everything. Serverless architecture for front end that is decentralized.  Also, on chain low level index eliminates external search tables.",
-      "photo":"url('world chain.png')",
-      //"button":"What can I do to help?"
+      "name":"True WEB3",
+      "description":"Digital transformation from 2.5 to 3.0 to manifest true WEB3, we decentralize everything. Serverless architecture for front end that is decentralized.  Also, on chain low level index eliminates external search tables.",
+      "photo":"url('worldchain.png')",
     },
     {
-      "name":"BENEFITS BENEFITS ",  // benefits
-      //"body":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
+      "name":"Friend with Benefits", 
       "description":"- On chain versioning, Apply bug fixes simply, Improve production speed",
       "photo":"url('benefits.png')",
-      //"button":"Learn about blockchain"
     }
     {
-      "name":"Real WEB3 now!",  // benefits
-      //"body":"With your provided starter kit, you have a simple cryptocurrency wallet, among other things. Use the 'Scan QR Codes' button above to start.",
+      "name":"Real WEB3 now!", 
       "description":"We're using real WEB3 now",
       "photo":"url('pinnochio.png')",
-      //"button":"Learn about blockchain"
+    }
+    {
+      "name":"A FEW KISSes",
+      "description":"Keep It Super Simple.  The world could use a FEW more KISSes",
+      "photo":"url('fewkiss.png')",
     }
   ]
 }
