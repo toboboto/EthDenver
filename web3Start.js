6172cf05f1f7852c18999f9fd807177019beb474
toboboto <toboboto@gmail.com>
2024-03-02 01:12:43 -0700

    Update web3Start.js.2
    part 2 done

--- web3Start.js
+++ web3Start.js
@@ -3,22 +3,22 @@
   "heading": "Intro",
   "slides": [
     {
-      "name": "Simply Radical Decentralization"//"Decentralization X Simplicity", // kiss FEW forever wall
+      "name": "Simply Radical Decentralization"// summary "Decentralization X Simplicity", // kiss FEW forever wall
       "description": "",
       "photo": "url('world.png?asd')",
       "url": "https://ipfs.io/ipfs/QmaMzwPXUGviEYk3uu2CqPBGnUmBgXFxRP8kJXFbbjSnCq",
       "button": "Access and save data with hashtags into the blockchain in teh most decentralized way possible" //few kiss
     },
     {
-      "name": "It's as easy as 1,2,3.",
+      "name": "We're building in web 2.5", //what is problem
       "description": "Scan your special code stamps into a secure website.",
       "body": "Within the tiny Web3 'early adopter' community, special Self-Sovereign software is used. A package called Metamask(metamask.io) is quite popular. But most people are not interested in installing more applications onto their phone or computer. That is why we developed this easy QR code system. We believe that these identity kits (with little QR code stickers) are just as secure as the industry standard solutions. They also allow you to experience the new Web3 without committing to a new piece of software.",
       "photo": "url('qrcode.jpg?23')",
       "url": "chick-image.html",
-      "button": "Learn about self-service identity."
+      "button": "WEB 2.5's use of centralized elements in front end and data servers hinders the full promise of blockchain."
     },
     {
-      "name":"How can this site be trusted?",
+      "name":"How can this site be trusted?", // why is a problem
       "description":"This is a Web3 hosted site. The URL that you are visiting should begin with 'ipfs.io'. ",
       "body":"We are used to businesses and people asking us to trust them based on some record of good behaviour. The Interplanetary Filesystem (IPFS) is an example of a 'trustless' Web3 technology. You have likely been trained to only visit and trust HTTPS websites. You can look at your browser to see this. IPFS takes this one step further. An IPFS website (https://ipfs.io/) uses a new 'Content-Based' addressing system. So if one character on this page is changed, the entire address will change.",
       "photo":"url('world.png')",
